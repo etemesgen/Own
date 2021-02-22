d54c81aa93103e9d6c0cf27d1cf1737dacbeda71
Edomiyas Temesgen <temesgen.edomiyas@yahoo.com>
2021-02-22 19:41:59 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+/* Changing color */ 
+const background = document.querySelector('h1');
+
+function changeColor(){
+    background.style.color = 'yellow';
+}
+
+changeColor();
+
+
+
+/* Display input 
+const prenom = prompt("Entrez votre prénom: ");
+alert(`Bonjour, ${prenom}`);
+*/
