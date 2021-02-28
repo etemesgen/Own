13b26ba65fe555b91f70788657b2c560c60de87f
Edomiyas Temesgen <temesgen.edomiyas@yahoo.com>
2021-02-28 19:55:28 +0100

    Revising classes, constructor and objects and trying to display them in the DOM

--- app.js
+++ app.js
@@ -13,3 +13,22 @@ const prenom = prompt("Entrez votre prénom: ");
 alert(`Bonjour, ${prenom}`);
 */
 
+/* Creating a class */
+class myShoes{
+    constructor(name, size, brand){
+        this.name = name,
+        this.size = size,
+        this.brand = brand
+    }
+}
+
+let firstShoes = new myShoes("AirForce", 43, "Nike"); 
+console.log(firstShoes);
+let secondShoes = new myShoes("Jordan mid 1", 44, "Jordan");
+console.log(secondShoes);
+
+/* Display the objects in the DOM*/
+var str = JSON.stringify(firstShoes);
+console.log(str);
+var displayShoes = document.getElementsByClassName("shoes").innerHTML = str;
+//console.log(displayShoes);
