63ad384bbc90049db9c0453c28288e0747e02def
Edomiyas Temesgen <temesgen.edomiyas@yahoo.com>
2021-03-30 03:53:34 +0200

    Revised IF/ELSE, EXCEPTIONS, DOM

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+})
 /* Changing color  
 const background = document.querySelector('h1');
 
@@ -14,6 +17,7 @@ alert(`Bonjour, ${prenom}`);
 */
 
 /* Creating a class */
+/*
 class myShoes{
     constructor(name, size, brand){
         this.name = name,
@@ -26,9 +30,46 @@ let firstShoes = new myShoes("AirForce", 43, "Nike");
 console.log(firstShoes);
 let secondShoes = new myShoes("Jordan mid 1", 44, "Jordan");
 console.log(secondShoes);
+*/
 
 /* Display the objects in the DOM*/
+/*
 var str = JSON.stringify(firstShoes);
 console.log(str);
 var displayShoes = document.getElementsByClassName("shoes").innerHTML = str;
+*/
 //console.log(displayShoes);
+/*console.log(typeof this.size); Precises type of object*/
+
+/* IF and ELSE statements */
+// User login check
+/*
+let user = "Edomiyas";
+let enter;
+
+if(user){
+    enter = 1;
+}
+else{
+    enter = 0;
+}
+console.log(enter);
+*/
+
+/* Iteration exercise FOR */
+/*
+let produit = 6;
+
+for(let iteration = 0; iteration <= 6; iteration++){
+    console.log(iteration);
+}
+console.log("This is the total number of product : " + produit);
+*/
+
+/* Exception Handling TRY CATCH*/
+
+// 3 types d'erreurs : erreur de syntaxe, erreur logique (if/while...), erreur d'éxecution(try/catch)
+
+/* DOM MANIPULATING */
+let displayShoes = document.getElementById("name").innerHTML = "Nike";
+
